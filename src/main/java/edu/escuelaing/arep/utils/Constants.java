92a9f818f52eec20ab426eb1c7b897cb99d55d38
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-02-15 14:17:41 -0500

    Adding constants

---
+++ src/main/java/edu/escuelaing/arep/utils/Constants.java
@@ -0,0 +1,10 @@
+package edu.escuelaing.arep.utils;
+
+/**
+ * @author Iván Camilo Rincón Saavedra
+ * @version 1.0 2/15/2022
+ * @project App
+ */
+public class Constants {
+    public final static String PUBLIC = "/public/";
+}
